f50c360d7cda94e9cbfd21e96b2c07c379b4ae15
Bryan Ngo <bngo92@gmail.com>
2013-03-28 10:40:28 -0700

    Added modular files

---
+++ carousel.php
@@ -0,0 +1,30 @@
+<div id="this-carousel-id" class="carousel slide">
+  <div class="carousel-inner">
+	<div class="item active">
+	  <img src="img/pic1.jpg" alt="" />
+	  <div class="carousel-caption">
+		<p>Caption text here</p>
+	  </div>
+	</div>
+	<div class="item">
+	  <img src="img/pic2.jpg" alt="" />
+	  <div class="carousel-caption">
+		<p>Caption text here</p>
+	  </div>
+	</div>
+	<div class="item">
+	  <img src="img/pic3.jpg" alt="" />
+	  <div class="carousel-caption">
+		 <p>Caption text here</p>
+	  </div>
+	</div>
+	<div class="item">
+	  <img src="img/pic4.jpg" alt="" />
+	  <div class="carousel-caption">
+		<p>Caption text here</p>
+	  </div>
+	</div>
+  </div>
+  <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+</div>
---
+++ navbar.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+  <div class="navbar-inner">
+	<a class="brand" href="#">UCLA Tau Beta Pi</a>
+	<ul class="nav">
+	  <li class="active"><a href="#">Home</a></li>
+	  <li><a href="#">Events</a></li>
+	  <li><a href="#">Requirements</a></li>
+	  <li><a href="#">Tutoring</a></li>
+	  <li><a href="#">About Us</a></li>
+	  <li><a href="#">Houses</a></li>
+	</ul>
+  </div>
+</div>
