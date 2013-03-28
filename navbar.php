8221ea4f4cac82a43cf4c191b5e92dd5d3508e89
Bryan Ngo <bngo92@gmail.com>
2013-03-28 10:59:24 -0700

    Moved login form

--- navbar.php
+++ navbar.php
@@ -8,14 +8,12 @@
 	  <li><a href="#">Tutoring</a></li>
 	  <li><a href="#">About Us</a></li>
 	  <li><a href="#">Houses</a></li>
+	  
+      <form class="form-inline">
+        <input type="text" class="input-small" placeholder="Email">
+        <input type="password" class="input-small" placeholder="Password">
+        <button type="submit" class="btn">Sign in</button>
+      </form>
 	</ul>
   </div>
 </div>
-<form class="form-inline">
-  <input type="text" class="input-small" placeholder="Email">
-  <input type="password" class="input-small" placeholder="Password">
-  <label class="checkbox">
-    <input type="checkbox"> Remember me
-  </label>
-  <button type="submit" class="btn">Sign in</button>
-</form>
