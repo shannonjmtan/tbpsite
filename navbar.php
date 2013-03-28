28eb8cf1772d2fa343653d04d7e9bb8348cef8e4
Bryan Ngo <bngo92@gmail.com>
2013-03-28 10:57:11 -0700

    Added login form

--- navbar.php
+++ navbar.php
@@ -11,3 +11,11 @@
 	</ul>
   </div>
 </div>
+<form class="form-inline">
+  <input type="text" class="input-small" placeholder="Email">
+  <input type="password" class="input-small" placeholder="Password">
+  <label class="checkbox">
+    <input type="checkbox"> Remember me
+  </label>
+  <button type="submit" class="btn">Sign in</button>
+</form>
