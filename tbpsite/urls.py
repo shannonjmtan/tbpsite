5015abfbefcb56abc7c13ece15712a5f97102586
Bryan Ngo <bngo92@gmail.com>
2013-03-30 23:32:28 -0700

    Ported website

--- tbpsite/urls.py
+++ tbpsite/urls.py
@@ -8,6 +8,18 @@ import tbpsite
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'tbpsite.views.home', name='home'),
+    url(r'^poker_tournament$', 'tbpsite.views.poker_tournament'),
+    url(r'^rubegoldberg$', 'tbpsite.views.rube_goldberg'),
+    url(r'^requirements$', 'tbpsite.views.requirements'),
+    #url(r'^candidates$', 'tbpsite.views.candidates'),
+    #url(r'^distinguishedactive$', 'tbpsite.views.distinguishedactive'),
+    url(r'^awards$', 'tbpsite.views.awards'),
+    url(r'^officers$', 'tbpsite.views.officers'),
+    url(r'^faculty$', 'tbpsite.views.faculty'),
+    url(r'^tutoring$', 'tbpsite.views.tutoring'),
+    url(r'^contact$', 'tbpsite.views.contact'),
+    url(r'^eligibility_list$', 'tbpsite.views.eligibility_list'),
+    url(r'^houses$', 'tbpsite.views.houses'),
     # url(r'^tbpsite/', include('tbpsite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- tbpsite/views.py
+++ tbpsite/views.py
@@ -2,3 +2,39 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def poker_tournament(request):
+    return render(request, 'poker_tournament.html')
+
+def rube_goldberg(request):
+    return render(request, 'rube_goldberg.html')
+
+def requirements(request):
+    return render(request, 'requirements.html')
+
+def tutoring(request):
+    return render(request, 'tutoring.html')
+
+def awards(request):
+    return render(request, 'awards.html')
+
+def officers(request):
+    return render(request, 'officers.html')
+
+def faculty(request):
+    return render(request, 'faculty.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def eligibility_list(request):
+    return render(request, 'eligibility_list.html')
+
+#def candidates(request):
+    #return render(request, 'candidates.html')
+
+#def distinguishedactive(request):
+    #return render(request, 'distinguishedactive.html')
+
+def houses(request):
+    return render(request, 'houses.html')
