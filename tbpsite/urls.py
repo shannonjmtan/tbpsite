79d0a0c4f4a6423e1268cdd0a2ecf9cd4c0b6e89
Bryan Ngo <bngo92@gmail.com>
2013-03-31 18:17:52 -0700

    Added login and logout

--- tbpsite/urls.py
+++ tbpsite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 import tbpsite
+import django
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -30,4 +31,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout$', 'tbpsite.views.logout'),
+    url(r'^login', 'tbpsite.views.login'),
 )
--- tbpsite/views.py
+++ tbpsite/views.py
@@ -1,55 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+def render_next(request, template_name):
+    return render(request, template_name, {'next': request.path})
 
 def home(request):
-    return render(request, 'home.html')
+    return render_next(request, 'home.html')
 
 def events(request): 
-    return render(request, 'events.html')
+    return render_next(request, 'events.html')
 
 def poker_tournament(request):
-    return render(request, 'poker_tournament.html')
+    return render_next(request, 'poker_tournament.html')
 
 def rube_goldberg(request):
-    return render(request, 'rube_goldberg.html')
+    return render_next(request, 'rube_goldberg.html')
 
 def requirements(request):
-    return render(request, 'requirements.html')
+    return render_next(request, 'requirements.html')
 
 def tutoring(request):
-    return render(request, 'tutoring.html')
+    return render_next(request, 'tutoring.html')
 
 def programs(request):
-    return render(request, 'programs.html')
+    return render_next(request, 'programs.html')
 
 def emcc(request):
-    return render(request, 'emcc.html')
+    return render_next(request, 'emcc.html')
 
 def fe(request):
-    return render(request, 'fe.html')
+    return render_next(request, 'fe.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render_next(request, 'about.html')
 
 def awards(request):
-    return render(request, 'awards.html')
+    return render_next(request, 'awards.html')
 
 def officers(request):
-    return render(request, 'officers.html')
+    return render_next(request, 'officers.html')
 
 def faculty(request):
-    return render(request, 'faculty.html')
+    return render_next(request, 'faculty.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render_next(request, 'contact.html')
 
 def eligibility_list(request):
-    return render(request, 'eligibility_list.html')
-
-#def candidates(request):
-    #return render(request, 'candidates.html')
-
-#def distinguishedactive(request):
-    #return render(request, 'distinguishedactive.html')
+    return render_next(request, 'eligibility_list.html')
 
 def houses(request):
-    return render(request, 'houses.html')
+    return render_next(request, 'houses.html')
+
+def login(request):
+    next = request.GET.get('next', False)
+    if not next:
+        next = '/'
+    username = request.POST.get('username', False)
+    password = request.POST.get('password', False)
+    if username and password:
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+    return redirect(next)
+
+def logout(request):
+    next = request.GET.get('next', False)
+    if not next:
+        next = '/'
+    auth.logout(request)
+    return redirect(next)
+
