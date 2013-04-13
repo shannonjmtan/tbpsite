94daddcfd18b9f7c1a614aa22650804e418c9730
Bryan Ngo <bngo92@gmail.com>
2013-04-12 23:35:22 -0700

    Separated web application

--- tbpsite/urls.py
+++ tbpsite/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-import main
 import django
 
 # Uncomment the next two lines to enable the admin:
@@ -8,33 +7,33 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'main.views.home', name='home'),
-    url(r'^events$', 'main.views.events'),
-    url(r'^poker_tournament$', 'main.views.poker_tournament'),
-    url(r'^rubegoldberg$', 'main.views.rube_goldberg'),
-    url(r'^requirements$', 'main.views.requirements'),
-    url(r'^programs$', 'main.views.programs'),
-    url(r'^emcc$', 'main.views.emcc'),
-    url(r'^fe$', 'main.views.fe'),
-    url(r'^about$', 'main.views.about'),
-    url(r'^awards$', 'main.views.awards'),
-    url(r'^officers$', 'main.views.officers'),
-    url(r'^faculty$', 'main.views.faculty'),
-    url(r'^tutoring$', 'main.views.tutoring'),
-    url(r'^feedback$', 'main.views.feedback'),
-    url(r'^contact$', 'main.views.contact'),
-    url(r'^eligibility_list$', 'main.views.eligibility_list'),
-    url(r'^houses$', 'main.views.houses'),
-    # url(r'^app/', include('main.foo.urls')),
+    url(r'^$', 'web.views.home', name='home'),
+    url(r'^events$', 'web.views.events'),
+    url(r'^poker_tournament$', 'web.views.poker_tournament'),
+    url(r'^rubegoldberg$', 'web.views.rube_goldberg'),
+    url(r'^requirements$', 'web.views.requirements'),
+    url(r'^programs$', 'web.views.programs'),
+    url(r'^emcc$', 'web.views.emcc'),
+    url(r'^fe$', 'web.views.fe'),
+    url(r'^about$', 'web.views.about'),
+    url(r'^awards$', 'web.views.awards'),
+    url(r'^officers$', 'web.views.officers'),
+    url(r'^faculty$', 'web.views.faculty'),
+    url(r'^tutoring$', 'web.views.tutoring'),
+    url(r'^feedback$', 'web.views.feedback'),
+    url(r'^contact$', 'web.views.contact'),
+    url(r'^eligibility_list$', 'web.views.eligibility_list'),
+    url(r'^houses$', 'web.views.houses'),
+    # url(r'^app/', include('web.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^logout$', 'main.views.logout'),
-    url(r'^login$', 'main.views.login'),
-    url(r'^profile$', 'main.views.profile'),
-    url(r'^resume$', 'main.views.resume'),
-    url(r'^interview$', 'main.views.interview'),
+    url(r'^logout$', 'web.views.logout'),
+    url(r'^login$', 'web.views.login'),
+    url(r'^profile$', 'web.views.profile'),
+    url(r'^resume$', 'web.views.resume'),
+    url(r'^interview$', 'web.views.interview'),
 )
