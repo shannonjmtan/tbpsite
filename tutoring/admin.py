1672fcdb3021b3e58676689d4a76fd68dd71de3b
TBP Webmaster <tbpofficer@tbp-server.tbp.seas.ucla.edu>
2013-05-16 23:06:28 -0700

    Modified tutoring table

--- tutoring/admin.py
+++ tutoring/admin.py
@@ -1,21 +1,32 @@
 from django.contrib import admin
 from tutoring.models import *
 
+def fill_hours(modeladmin, request, queryset):
+    for member in queryset:
+        member.hours = TUTORING_HOURS_PER_WEEK
+        member.save()
+
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('timestamp', 'comment') 
     def __init__(self, *args, **kwargs):
         super(FeedbackAdmin, self).__init__(*args, **kwargs)
         self.list_display_links = (None,)
 
+class ClassAdmin(admin.ModelAdmin):
+    pass
+
 class TutoringAdmin(admin.ModelAdmin):
+    fields = ('day_1', 'hour_1', 'day_2', 'hour_2', 'classes')
     list_display = ('__unicode__', 'term', 'day_1', 'hour_1', 'day_2', 'hour_2')
     list_editable = ('day_1', 'hour_1', 'day_2', 'hour_2')
 
 class WeekAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'term', 'day_1', 'hour_1', 'day_2', 'hour_2', 'hours', 'tutees')
     list_editable = ('hours', 'tutees')
+    actions = (fill_hours,)
     
 admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Class, ClassAdmin)
 admin.site.register(Tutoring, TutoringAdmin)
 admin.site.register(Week3, WeekAdmin)
 admin.site.register(Week4, WeekAdmin)
